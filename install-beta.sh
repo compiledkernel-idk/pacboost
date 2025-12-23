08e1059af27121177b492af68f9cddf8d4cc8b68
compiledkernel-idk <berkkapla82@gmail.com>
2025-12-23 23:34:02 +0100

    fix: use temp dir in install script to avoid conflicts

--- install-beta.sh
+++ install-beta.sh
@@ -3,18 +3,29 @@ set -e
 
 echo ":: Installing pacboost (BETA v1.2.0-beta)..."
 
+# Create a temporary directory to avoid conflicts
+TEMP_DIR=$(mktemp -d)
+cd "$TEMP_DIR"
+
 # Download the tarball
-curl -L -o pacboost.tar.gz https://github.com/compiledkernel-idk/pacboost/releases/download/v1.2.0-beta/pacboost-x86_64-linux.tar.gz
+echo ":: Downloading..."
+curl -sL -o pacboost.tar.gz https://github.com/compiledkernel-idk/pacboost/releases/download/v1.2.0-beta/pacboost-x86_64-linux.tar.gz
 
 # Extract
+echo ":: Extracting..."
 tar -xzf pacboost.tar.gz
 
 # Install
 echo ":: Installing binary..."
-sudo mv pacboost /usr/local/bin/
+if [ "$EUID" -ne 0 ]; then
+  sudo mv pacboost /usr/local/bin/
+else
+  mv pacboost /usr/local/bin/
+fi
 
 # Cleanup
-rm pacboost.tar.gz
+cd - > /dev/null
+rm -rf "$TEMP_DIR"
 
 echo ":: pacboost beta installed successfully!"
 pacboost --version
